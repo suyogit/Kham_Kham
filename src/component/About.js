df79d9a20866d25ff4c37ad14ded44a86cd13c3d
Suyog Acharya <asuyog042@gmail.com>
2023-07-27 16:13:03 +0545

    seeing the lifecycle of class components

--- src/component/About.js
+++ src/component/About.js
@@ -1,7 +1,52 @@
-//import { Outlet } from "react-router-dom";
-import Profile from "./Profile"
+// //import { Outlet } from "react-router-dom";
+// import Profile from "./Profile"
+// import ProfileClass from "./ProfileClass";
+// const About = () => {
+//     return (
+//         <div className="about-container">
+//             <div className="about-left">
+//                 <h1>
+//                     Welcome to <br /> The world of <br /> <span>Tasty & Fresh Food</span>
+//                 </h1>
+//                 <h4>
+//                     "Better you will feel if you eat a Kham<span>Kham</span> healthy meal"
+//                 </h4>
+//         </div>
+//             <div className="about-right">
+//                 <img src="" alt="Food Image" />
+//             </div>
+//             {/* <Outlet /> */}
+//             <Profile name={"suyog funcitonal"} />
+//             <ProfileClass name={"suyyoog class"} />
+//         </div>
+//     );
+// };
+
+// export default About;
+
+
 import ProfileClass from "./ProfileClass";
-const About = () => {
+
+import React, { Component } from 'react'
+
+export default class About extends Component {
+
+
+    constructor(props) {
+        super(props)
+        console.log("constructor parent")
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount parent")
+    }
+
+
+    render() {
+
+        console.log("render parent")
+
+
     return (
         <div className="about-container">
             <div className="about-left">
@@ -11,15 +56,16 @@ const About = () => {
                 <h4>
                     "Better you will feel if you eat a Kham<span>Kham</span> healthy meal"
                 </h4>
-        </div>
+            </div>
             <div className="about-right">
                 <img src="" alt="Food Image" />
             </div>
             {/* <Outlet /> */}
-            <Profile name={"suyog funcitonal"} />
-            <ProfileClass name={"suyyoog class"} />
-        </div>
-    );
-};
 
-export default About;
+            <ProfileClass name={"suyyoog 1st class"} />
+            <ProfileClass name={"suyyoog 2nd class"} />
+        </div>
+        )
+    }
+}
+
--- src/component/ProfileClass.js
+++ src/component/ProfileClass.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ProfileClass extends Component {
+
+    constructor(props) {
+        super(props)
+        console.log("constructor child" + this.props.name)
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount child" + this.props.name)
+    }
+
+
     render() {
+
+        console.log("render child" + this.props.name)
+
         return (
             <div>
                 <div>ProfileClass</div>
